95d44b076919ff17d1086c3b4a10c96cdd6b7770
Pete Schuster <pas5027@gmail.com>
2017-10-12 19:47:04 -0400

    adds post template

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './containers/pages/Home'
+import Post from './containers/templates/Post'
 
 export const Routes = () => (
   <div>
     <Route exact path='/' component={Home} />
+    <Route path='/:year/:month/:slug' component={Post} />
   </div>
 )
 
