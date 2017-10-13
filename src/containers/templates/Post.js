7379a661d370165e2018ab556a1303460fabe871
Pete Schuster <pas5027@gmail.com>
2017-10-12 20:00:06 -0400

    renders post

--- src/containers/templates/Post.js
+++ src/containers/templates/Post.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 class Post extends Component {
+  componentDidMount() {
+    let slug = this.props.match.params.slug;
+
+    let dataURL = `https://peteschuster.com/wp-json/wp/v2/posts?slug=${slug}`;
+    fetch(dataURL)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          post: res[0]
+        })
+      })
+  }
+
   render() {
+    const { post } = this.state;
+
+    if (!post) return null;
+
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>{post.title.rendered}</h1>
       </div>
     )
   }
