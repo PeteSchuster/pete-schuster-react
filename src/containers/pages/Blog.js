c1e773082a08bc12e354d5ea62e84c272ab77b65
Pete Schuster <pas5027@gmail.com>
2017-10-12 21:29:47 -0400

    set blog

--- src/containers/pages/Blog.js
+++ src/containers/pages/Blog.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Blog extends Component {
+  componentDidMount() {
+    let dataURL = `https://peteschuster.com/wp-json/wp/v2/posts`;
+    fetch(dataURL)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          posts: res
+        })
+      })
+  }
+
+  listPosts(posts) {
+    const items = posts.map(post => {
+      return (
+        <div class="post">
+          <Link to={post.link}>{post.title.rendered}</Link>
+        </div>
+      );
+    });
+
+    return (
+      <div class="posts">
+        {items}
+      </div>
+    )
+  }
+
   render() {
+    const { posts } = this.state;
+
+    if (!posts) return `loading...`;
+
     return (
       <div>
-        <h1>Blog</h1>
+        {this.listPosts(posts)}
       </div>
     )
   }
