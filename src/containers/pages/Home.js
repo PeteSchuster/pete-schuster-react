1d6ded960c9c691d77e1f94afe5850cc8fc9a672
Pete Schuster <pas5027@gmail.com>
2017-10-29 08:25:21 -0400

    use white

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -3,7 +3,6 @@ import PageSection from '../../components/common/PageSection'
 import Triangle from '../../components/svgs/Triangle'
 import Circle from '../../components/svgs/Circle'
 import Star from '../../components/svgs/Star'
-import Building from '../../components/svgs/Building'
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -32,35 +31,16 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <PageSection className="ui-blue-midnight">
-          <h6>About Me</h6>
-          <h1>Philadelphia Freelance WordPress Developer</h1>
-
-          <p>With a firm understanding of both front end web development and the WordPress platform, I can help you take your project to the next level. Keeping up to date on the trends of user interface and user experience design, creating feature rich websites that are easy to maintain and easy to manage is what I do best. Be sure to think of me when you start designing your next project.</p>
-
-          {this.splatter(<Triangle />)}
-        </PageSection>
         <PageSection className="ui-white-powder">
           <h6>About Me</h6>
           <h1>Philadelphia Freelance WordPress Developer</h1>
 
           <p>With a firm understanding of both front end web development and the WordPress platform, I can help you take your project to the next level. Keeping up to date on the trends of user interface and user experience design, creating feature rich websites that are easy to maintain and easy to manage is what I do best. Be sure to think of me when you start designing your next project.</p>
-          {this.splatter(<Circle />, 'red')}
-        </PageSection>
-        <PageSection className="ui-yellow-crayon">
-          <h6>About Me</h6>
-          <h1>Philadelphia Freelance WordPress Developer</h1>
 
-          <p>With a firm understanding of both front end web development and the WordPress platform, I can help you take your project to the next level. Keeping up to date on the trends of user interface and user experience design, creating feature rich websites that are easy to maintain and easy to manage is what I do best. Be sure to think of me when you start designing your next project.</p>
-          {this.splatter(<Star />, 'blue')}
         </PageSection>
-        <PageSection className="ui-yellow-crayon">
-          <h6>About Me</h6>
-          <h1>Philadelphia Freelance WordPress Developer</h1>
-
-          <p>With a firm understanding of both front end web development and the WordPress platform, I can help you take your project to the next level. Keeping up to date on the trends of user interface and user experience design, creating feature rich websites that are easy to maintain and easy to manage is what I do best. Be sure to think of me when you start designing your next project.</p>
-          {this.splatter(<Building />, 'blue')}
-        </PageSection>
+        {this.splatter(<Triangle />, 'green')}
+        {this.splatter(<Circle />, 'red')}
+        {this.splatter(<Star />, 'blue')}
       </div>
     )
   }
