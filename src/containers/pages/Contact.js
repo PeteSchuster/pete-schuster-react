2e46af3148470bbd10109a8ed11dd280f6ccf536
Pete Schuster <pas5027@gmail.com>
2017-11-16 14:19:55 -0500

    sets states for fields

--- src/containers/pages/Contact.js
+++ src/containers/pages/Contact.js
@@ -4,11 +4,14 @@ import PageSection from '../../components/common/PageSection'
 class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.initialState = {
       name: '',
       email: '',
-      body: ''
+      body: '',
+      loading: false
     };
+
+    this.state = Object.assign({}, this.initialState);
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -31,25 +34,70 @@ class Contact extends Component {
 
     let url = `https://script.google.com/macros/s/AKfycbxdTx1uqNG8fyOME73_2zpysfeH5oUbPRmszSweFaXj-FtakQI/exec?${query}`;
 
+    this.setState({ loading: true });
+
     fetch(url, {
       method: 'GET',
       headers : new Headers()
     }).then(res => {
       console.log(res);
+      this.setState(Object.assign({}, this.initialState, {success: true}));
     });
   }
 
+  renderSuccess() {
+    return (
+      <div>
+        Success!
+      </div>
+    )
+  }
+
+  renderLoading() {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
   render() {
+    const {success, loading} = this.state;
+
     return (
       <PageSection className="ui-white-powder">
         <h1>Contact</h1>
 
+        {success && this.renderSuccess()}
+        {loading && this.renderLoading()}
+
         <form onSubmit={this.handleSubmit}>
-          <input id="name" type="text" name="name" onChange={this.handleChange} />
+          <label for="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+            required />
 
-          <input id="email" type="email" name="email" onChange={this.handleChange} />
+          <label for="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+            required />
 
-          <textarea id="body" name="body" onChange={this.handleChange}></textarea>
+          <label for="body">Message</label>
+          <textarea
+            id="body"
+            name="body"
+            onChange={this.handleChange}
+            value={this.state.body}
+            required>
+          </textarea>
 
           <button type="submit">Submit</button>
         </form>
